8a468f91584b8f8e0e5d6d49b388a2a6fe71f039
singhkunal30 <singhkunal30@outlook.com>
2024-02-20 02:02:06 +0530

    Basic project structure

---
+++ src/main/java/com/crm/controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.crm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.crm.dto.CustomerDTO;
+import com.crm.service.CustomerService;
+import com.crm.utils.JsonUtils;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class CustomerController {
+	
+	@Autowired
+	CustomerService customerService;
+	
+	@PostMapping("/add")	
+	public ResponseEntity<CustomerDTO> createCustomer(HttpServletRequest request) {
+		CustomerDTO customerDTO = JsonUtils.bindRequestToObject(request, new TypeReference<CustomerDTO>() {});
+		return new ResponseEntity<>(customerService.createCustomer(customerDTO), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/get/{id}")
+	public ResponseEntity<CustomerDTO> getCustomer(@PathVariable int id) {
+		return new ResponseEntity<>(customerService.getCustomer(id), HttpStatus.OK);
+	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<CustomerDTO> updateCustomer(HttpServletRequest request) {
+		CustomerDTO customerDTO = JsonUtils.bindRequestToObject(request, new TypeReference<CustomerDTO>() {});
+		return new ResponseEntity<>(customerService.updateCustomer(customerDTO), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> removeCustomer(@PathVariable int id) {
+		return new ResponseEntity<>(customerService.removeCustomer(id), HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/crm/exception/CustomerException.java
@@ -0,0 +1,23 @@
+package com.crm.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@SuppressWarnings("serial")
+@Getter
+public class CustomerException extends RuntimeException{
+	private final LocalDateTime timestamp;
+	private final String errMsg;
+	private final Long errCode;
+	private final HttpStatus httpStatus;
+	
+	public CustomerException(String errMsg, Long errCode, HttpStatus httpStatus) {
+		this.timestamp = LocalDateTime.now();
+		this.errMsg = errMsg;
+		this.errCode = errCode;
+		this.httpStatus = httpStatus;
+	}
+}
