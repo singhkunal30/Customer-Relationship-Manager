89cd036f2dbf7c3eef65e0863e145a95691c61fa
singhkunal30 <singhkunal30@outlook.com>
2024-02-23 01:31:07 +0530

    Just commiting to fix local repos 2

---
+++ src/main/java/com/crm/security/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.crm.security;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.RSAKey;
+
+@Configuration
+public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.
+			authorizeHttpRequests((authz)->
+				authz.anyRequest().authenticated()
+			)
+			.httpBasic(Customizer.withDefaults())
+			.csrf((csrf) -> csrf.disable())
+			.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()))
+			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+		return http.build();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public KeyPair keyPair() {
+		try {
+			var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Bean
+	public RSAKey rsaKey(KeyPair keyPair) {
+		return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+			.privateKey(keyPair.getPrivate())
+			.keyID(UUID.randomUUID().toString())
+			.build();
+	}
+}
